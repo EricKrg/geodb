d37371a916b0484c71fa3d0ad175b21b88b03947
EricKrg <eric.krueger@uni-jena.de>
2017-11-09 15:48:30 +0100

    changed to real date format

--- R_skripte/tages_werte.R
+++ R_skripte/tages_werte.R
@@ -7,7 +7,7 @@
 
 require("RPostgreSQL")
 
-pacman::p_load(dplyr, tidyverse)
+pacman::p_load(dplyr, tidyverse, lubridate)
 
 # create a connection
 # save the password that we can "hide" it as best as we can by collapsing it
@@ -24,18 +24,21 @@ con <- dbConnect(drv, dbname = "Messdaten",        #change con to elephantsql da
                  user = "postgres", password = pw)
 rm(pw) # removes the password
 
-qry <- {"select * from pegel_messdaten inner join messdaten on pegel_messdaten.did = messdaten.did"
+qry <- {"select * from pegel_messdaten"
   } #Pegelmessdaten query
 
 relevant <- dbGetQuery(con, qry)
-relevant[,4] <- NULL
 
-relevant <- cbind(relevant, "mon" = substr(relevant$datum, 5,6))
-relevant <- cbind(relevant, "day" = substr(relevant$datum, 7,8))
+relevant <- cbind(relevant, "date" = as.Date(paste0(substr(relevant$mess_datum,1,4),
+                                             "/", substr(relevant$mess_datum, 5,6),
+                                             "/", substr(relevant$mess_datum,7,8))))
+relevant<- as_tibble(relevant)
+
 
 ###neu----
 
 ##loop for daily values
+
 stations <- relevant %>%
   select(stations_id) %>%
   distinct  # df of stations, for later iteration
@@ -46,25 +49,25 @@ for (i in 1: NROW(stations)){   #break down to stations
                          subset(relevant, relevant$stations_id == stations$stations_id[i]))
 }
 
-days <- relevant %>%
-  select(day) %>%
+date<- relevant %>%
+  select(date) %>%
   distinct # df of days, for later iteration
+
 day_list <- list()
 
 i = 1
-for (q in days$day){   #iteration of daily values
+for (q in date){   #iteration of daily values
   print(q) #for day q
   for (j in 1:length(df_list) ){
   print(j)  #for station j
-  temp <- df_list[[j]][which(df_list[[j]]$day == q),,drop=F] # all values form station j on day q
+  temp <- df_list[[j]][which(df_list[[j]]$date == q),,drop=F] # all values form station j on day q
   durchfl <- sum(temp$durchfluss)/NROW(temp)  # mean daily values
   wassers <- sum(temp$wasserstand)/NROW(temp)
   station <- temp$stations_id[1]
-  mon <- temp$mon[1]
-  year <- substr(temp$datum[1],1,4 )
+  date <- q
   day_list[[i]] <- assign(paste0("day",temp$day),
                           tibble(durchfluss = durchfl, wasserstand = wassers,
-                          tag =as.numeric(q),mon = mon , station = station, year = year))
+                          station = station, date))
   i = i + 1
   }
 }
@@ -79,16 +82,17 @@ week_list = list()
 n = 1
 start = 0
 
-duration = seq(day_df$tag[1], max(day_df$tag), by = 7) # one week duration (not yet working for monthly change)
-for (a in duration){  # weekly iteration
-  start =a
+duration = as_tibble(seq(day_df$date[1], max(day_df$date), by = 7)) # one week duration (not yet working for monthly change)
+
+for (a in 1:NROW(duration)){  # weekly iteration
+  start = duration$value[a]
   print(start)#first day of the week
   for (k in stations){
     print(k)
     week_list[[n]]<- day_df %>%
       filter(station == k) %>%
-      filter(tag >= start) %>%
-      filter(tag <= start + 6) # filter one week, still includes shorter periodes then one week
+      filter(date >= start) %>%
+      filter(date <= start + days(6)) # filter one week, still includes shorter periodes then one week
     print("woche:")
     print(length(week_list))
     n = n + 1
@@ -105,16 +109,15 @@ for(e in week_list){  # values per week + writing to db
   print(e)
   durchfluss <- sum(e$durchfluss)/NROW(e)
   wasserstand <- sum(e$wasserstand)/NROW(e)
-  datum <- paste0(e$tag[1], "_", e$tag[NROW(e)], e$mon[1])
+  date <- paste0(e$date[1], "_", e$date[NROW(e)])
   station <- e$station[1]
-  year <- e$year[1]
   full_week <- FALSE
-  if (e$tag[1] - e$tag[NROW(e)] == -6 ){
+  if (e$date[1] + days(6) == e$date[NROW(e)] ){
     full_week <- TRUE
   }
-  final_values[[q]] <- assign(paste0(e$station[1],"_", datum, "_", year),      # maybe conditon to avoid double write
-               tibble(station, durchfluss, wasserstand, start = substr(datum,1,2),
-                      end = substr(datum,4,5 ),monat = substr(datum,6,7), full_week, year))
+  final_values[[q]] <- assign(paste0(e$station[1],"_", date),
+               tibble(station, durchfluss, wasserstand, start = e$date[1],
+                      end = e$date[nrow(e)],full_week))
   q = q + 1
   }
 
